9d4fb60a08d3912d3b4645f0f834cd8df493de58
Jarvis Yang <jarvisgyang@gmail.com>
2021-04-20 23:36:58 -0500

    Adding AppHeader component and test. Updating default layout to incorporate AppHeader.

---
+++ components/AppHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav class="navbar">
+    <h1>
+      <img
+        id="header-title-image"
+        src="~/assets/icon.png"
+        alt="Gray App Store Icon"
+      />
+      Add new User Profile
+    </h1>
+  </nav>
+</template>
+
+<style scoped>
+#header-title-image {
+  padding-right: 2%;
+}
+
+.navbar {
+  position: stick;
+  top: 0;
+  z-index: 1;
+  border-bottom: var(--header-border-bottom);
+}
+
+h1 {
+  color: #5b6983;
+  font-size: 2rem;
+  padding-top: 2%;
+  padding-bottom: 4%;
+  padding-left: 2%;
+}
+</style>
---
+++ test/AppHeader.spec.js
@@ -0,0 +1,9 @@
+import { mount } from '@vue/test-utils'
+import AppHeader from '~/components/AppHeader.vue'
+
+describe('AppHeader', () => {
+  test('is a Vue instance', () => {
+    const wrapper = mount(AppHeader)
+    expect(wrapper.vm).toBeTruthy()
+  })
+})
