58380bdb7cfc090fe50889fc9447603bb1a8b4f0
Jarvis Yang <jarvisgyang@gmail.com>
2021-04-21 02:09:47 -0500

    Setting up mock form for index page.

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,37 @@
 <template>
-  <div>
-    <h1>First Form Goes Here</h1>
+  <div class="specific-page">
+    <form>
+      <div class="form-group">
+        <label for="inputEmail">EMAIL</label>
+        <input
+          id="inputEmail"
+          type="email"
+          class="form-control"
+          aria-describedby="emailHelp"
+          placeholder="[Required]"
+        />
+      </div>
+      <div class="form-group">
+        <label for="inputPassword">PASSWORD</label>
+        <input
+          id="inputPassword"
+          type="password"
+          class="form-control"
+          placeholder="[Required]"
+        />
+      </div>
+      <div class="form-group">
+        <label for="verifyInputPassword">VERIFY PASSWORD</label>
+        <input
+          id="verifyInputPassword"
+          type="password"
+          class="form-control"
+          placeholder="[Required]"
+        />
+      </div>
+      <button type="reset" class="btn">Cancel</button>
+      <button type="submit" class="btn btn-primary">Next</button>
+    </form>
   </div>
 </template>
 
