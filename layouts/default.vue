be1477947624343aefe064445c9f02f050421e82
Jarvis Yang <jarvisgyang@gmail.com>
2021-04-21 01:07:04 -0500

    Rearranging default layout, and bringing in Proxima-Nova font.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,34 @@
 <template>
   <div>
     <AppHeader />
-    <b-row>
-      <b-col>
+    <main>
+      <div>
         <SideNav />
-      </b-col>
-      <b-col>
         <Nuxt />
-      </b-col>
-    </b-row>
+      </div>
+    </main>
   </div>
 </template>
 
 <style>
+@font-face {
+  font-family: 'Proxima-Nova';
+  src: url('~/assets/Proxima-Nova.ttf');
+}
+
 :root {
   --header-border-bottom: 1px solid#cfdee9;
 }
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Proxima-Nova';
+}
+
+/* main {
+  display: block;
+} */
 </style>
