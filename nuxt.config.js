4d4b7af78f6b39d6adc09d5287fb1762835796b0
Jarvis Yang <jarvisgyang@gmail.com>
2021-04-22 06:36:26 -0500

    Adding deploy script for gh-pages and configure for SPA.

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,6 +13,17 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
+  target: 'static',
+
+  router: {
+    base: '/ssr-user-registration-app/',
+  },
+
+  // Allows page refresh to work on github pages
+  generate: {
+    fallback: '404.html',
+  },
+
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
